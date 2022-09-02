9b6f15485d3c1b9060052a81d74b417d4af234a1
Michael Shen <electrycity84@gmail.com>
2022-09-02 11:42:22 +0800

    modify .env to .env.local

--- src/firebaseConfig.tsx
+++ src/firebaseConfig.tsx
@@ -2,8 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig:string | undefined = process.env.REACT_APP_FIREBASE_CONFIG;
+const firebaseConfig= process.env.REACT_APP_FIREBASE_CONFIG as unknown as object;
 
-const app = initializeApp(process.env.REACT_APP_FIREBASE_INITIALIZEApp as unknown as object);
+const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
